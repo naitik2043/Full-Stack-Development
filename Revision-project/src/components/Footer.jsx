a551885041207bb65da423d93a509b5be38cc314
naitik2043 <206251886+naitik2043@users.noreply.github.com>
2026-04-19 14:43:11 +0530

    Added Revision project

---
+++ Revision-project/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>© 2026 AchievoLab</p>
+      <p>Team LOOP</p>
+
+      <ul>
+        <li>Home</li>
+        <li>Features</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ Revision-project/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <nav>
+        <div>
+          <h2>AchievoLab</h2>
+          <p>Team LOOP</p>
+        </div>
+
+        <ul>
+          <li>Home</li>
+          <li>Features</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
