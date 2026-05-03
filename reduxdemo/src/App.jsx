723da2fb4be7ca462d9dc0a0930cd1706ad77ec6
naitik2043 <206251886+naitik2043@users.noreply.github.com>
2026-05-04 00:34:40 +0530

    Redux classes

--- reduxdemo/src/App.jsx
+++ reduxdemo/src/App.jsx
@@ -1,9 +1,18 @@
-import Todo from "./components/Todo";
+import Todo from "./components/Todo.jsx";
 
 export default function App() {
   return (
-    <div>
-      <h1>TODO APP with redux</h1>
+    <div
+      style={{
+        maxWidth: "600px",
+        margin: "0 auto",
+        padding: "20px",
+        fontFamily: "Arial, sans-serif",
+      }}
+    >
+      <h1 style={{ textAlign: "center", color: "#333" }}>
+        TODO APP with Redux
+      </h1>
       <Todo />
     </div>
   );
